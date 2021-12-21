7420b75874274cb6de10d5c5da9148dfd4cdd59f
Sahib Fermanli <87578481+sahibfermanli@users.noreply.github.com>
2021-12-21 12:36:12 +0400

    http://localhost/index.php returns simple json content

--- public/index.php
+++ public/index.php
@@ -1,5 +1,20 @@
 <?php
 
-echo 'public';
-
-phpinfo();
+$users = [
+    [
+        'name' => 'Sahib',
+        'surname' => 'Fermanli',
+        'birthday' => '1997-05-23',
+        'country' => 'Azerbaijan',
+        'is_developer' => true
+    ],
+    [
+        'name' => 'Mesut',
+        'surname' => 'Ozil',
+        'birthday' => '1988-09-15',
+        'country' => 'Turkey',
+        'is_developer' => false
+    ]
+];
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($users);
